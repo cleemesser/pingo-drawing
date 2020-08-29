2b27117af1f99ee169f1aa090dc684aa8a422909
chris lee-messer <chris@lee-messer.net>
2020-08-29 11:08:07 -0700

    add requirements file

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ from distutils.core import setup
 
 
 setup(
-    name="pingodraw",
+    name="pingo",
     version="0.1.0",
     description="""platform independent drawing""",
     author="""Chris Lee-Messer""",
     url="https://github.com/eegml/pingo-drawing",
     classifiers=["Topic :: Graphics"],
-    packages=["pingodraw"],
+    packages=["pingo"],
     #install_requires = [],
     # package_data={}
     # data_files=[],
